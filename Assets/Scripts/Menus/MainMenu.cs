fe064e93c349c04276bb166314193fc65473aef1
Evan Mastrantuono <xnaveman@gmail.com>
2025-03-18 15:13:02 +0100

    mainmenu hover fixes

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject panel_options;
+	[SerializeField] private GameObject panel_credits;
 	
 	void Start(){
 		UnshowOptions();
@@ -21,6 +22,14 @@ public class MainMenu : MonoBehaviour
 	
 	public void UnshowOptions(){
 		panel_options.SetActive(false);
+	}
+
+	public void ShowCredits(){
+		panel_credits.SetActive(true);
+	}
+
+	public void UnshowCredits(){
+		panel_credits.SetActive(false);
 	}
 	
 	public void QuitGame(){

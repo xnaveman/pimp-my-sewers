bb53a61b3e82868c8e524b373503f8af13f057f7
Evan Mastrantuono <xnaveman@gmail.com>
2025-03-18 14:43:10 +0100

    added placeholder chests for customization

---
+++ Assets/Scripts/Gameplay/GroundCustomization.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class GroundCustomization : MonoBehaviour
+{
+    private bool playerInRange = false;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            HudManager.instance.showMessage("Clic droit pour changer le sol");
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            HudManager.instance.eraseMessage();
+        }
+    }
+    
+    private void Update()
+    {
+        if(playerInRange && Input.GetMouseButtonDown(1))
+        {
+            HudManager.instance.eraseMessage();
+        }
+    }
+}
---
+++ Assets/Scripts/Gameplay/WallCustomization.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class WallCustomization : MonoBehaviour
+{
+    private bool playerInRange = false;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            HudManager.instance.showMessage("Clic droit pour changer les murs");
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            HudManager.instance.eraseMessage();
+        }
+    }
+    
+    private void Update()
+    {
+        if(playerInRange && Input.GetMouseButtonDown(1))
+        {
+            HudManager.instance.eraseMessage();
+        }
+    }
+}

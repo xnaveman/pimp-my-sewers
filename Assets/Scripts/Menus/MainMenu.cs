24b95b96bca5881f5d620cc395bd709b8e8f7b68
Evan Mastrantuono <xnaveman@gmail.com>
2025-03-18 16:14:58 +0100

    implemented all panels for mainmenu options

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -7,6 +7,13 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private GameObject panel_options;
 	[SerializeField] private GameObject panel_credits;
+
+	[SerializeField] private GameObject panel_controls;
+	[SerializeField] private GameObject panel_display;
+	[SerializeField] private GameObject panel_sound;
+	[SerializeField] private GameObject panel_language;
+
+
 	
 	void Start(){
 		UnshowOptions();
@@ -30,6 +37,38 @@ public class MainMenu : MonoBehaviour
 
 	public void UnshowCredits(){
 		panel_credits.SetActive(false);
+	}
+
+	public void ShowControls(){
+		panel_controls.SetActive(true);
+	}
+
+	public void UnshowControls(){
+		panel_controls.SetActive(false);
+	}
+
+	public void ShowDisplay(){
+		panel_display.SetActive(true);
+	}
+
+	public void UnshowDisplay(){
+		panel_display.SetActive(false);
+	}
+
+	public void ShowSound(){
+		panel_sound.SetActive(true);
+	}
+
+	public void UnshowSound(){
+		panel_sound.SetActive(false);
+	}
+
+	public void ShowLanguage(){
+		panel_language.SetActive(true);
+	}
+
+	public void UnshowLanguage(){
+		panel_language.SetActive(false);
 	}
 	
 	public void QuitGame(){

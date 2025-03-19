07b6f2f1fb3918a72e29e17e8f7d95e0eda500e8
AmelieSavaryTolstoy <tolstoy.a.savary@gmail.com>
2025-03-19 10:50:49 +0100

    slider fonctionel

--- Assets/Scripts/SliderScript.cs
+++ Assets/Scripts/SliderScript.cs
@@ -8,11 +8,17 @@ public class SliderScript : MonoBehaviour
 {
     [SerializeField] private Slider _slider;
     [SerializeField] private TextMeshProUGUI _sliderText;
+    [SerializeField] private Slider _slider1;
+    [SerializeField] private TextMeshProUGUI _sliderText1;
     // Start is called before the first frame update
     void Start()
     {
         _slider.onValueChanged.AddListener((v) => {
             _sliderText.text = v.ToString("000");
+        });
+
+        _slider1.onValueChanged.AddListener((v) => {
+            _sliderText1.text = v.ToString("000");
         });
     }
 

c7dc5acc13e8fc104f642a77c434bd3695182bf2
Val LOMBART <33480.val@gmail.com>
2025-03-19 09:30:10 +0100

    add SprayNumberQuest script for object detection and HUD messaging

---
+++ Assets/Scripts/Gameplay/Quests/SprayNumberQuest.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class SprayNumberQuest : MonoBehaviour
+{
+    // Maximum distance from the camera center to detect this object.
+    public float detectionDistance = 5f;
+    
+    private Camera mainCamera;
+    private bool messageShown = false;
+    
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        if(mainCamera == null)
+            return;
+        
+        // Create a ray from the center of the screen (viewport (0.5,0.5)).
+        Ray ray = mainCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        
+        // Cast a ray and check if it hits within the detection distance.
+        if (Physics.Raycast(ray, out hit, detectionDistance))
+        {
+            // Check if the object we hit is this object.
+            if (hit.collider != null && hit.collider.gameObject == gameObject)
+            {
+                if (!messageShown)
+                {
+                    HudManager.instance.showMessage("test spray");
+                    messageShown = true;
+                }
+                return;
+            }
+        }
+        
+        // If the ray doesn't hit this object, erase the message.
+        if (messageShown)
+        {
+            HudManager.instance.eraseMessage();
+            messageShown = false;
+        }
+    }
+}

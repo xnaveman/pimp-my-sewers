dced5369818adfa6d777e6d8ca385b5c364765c0
Evan Mastrantuono <xnaveman@gmail.com>
2025-03-19 11:49:23 +0100

    add trash pickup functionality and update HUD message handling

---
+++ Assets/Scripts/Gameplay/Pickups/TrashPickup.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+public class TrashPickup : MonoBehaviour
+{
+    private bool playerInRange = false;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = true;
+            HudManager.instance.showMessage("Clic droit pour ramasser");
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerInRange = false;
+            HudManager.instance.eraseMessage();
+        }
+    }
+    
+    private void Update()
+    {
+        if(playerInRange && Input.GetMouseButtonDown(1))
+        {
+            HudManager.instance.eraseMessage();
+            GameManager.instance.trashCollected++;
+            HudManager.instance.showTimedMessage("Poubelle ramassée (" + GameManager.instance.trashCollected + "/8)");
+            
+            // Destroy the pickup (and its parent if needed).
+            if(transform.parent != null)
+                Destroy(transform.parent.gameObject);
+            Destroy(gameObject);
+            
+            // Use the persistent HudManager to remove the message after 2 seconds.
+            HudManager.instance.RemoveMessageAfterDelay(2f);
+        }
+    }
+}
